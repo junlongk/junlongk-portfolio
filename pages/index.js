c36319252160b6fc79b5e8f321478b0a026beaf7
Jun <keejunlong@hotmail.com>
2022-06-27 23:54:12 +0800

    Fixed some errors on ESLint warnings

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Jun Long's Portfolio</title>
+        <title>Jun Long&apos;s Portfolio</title>
         <meta name="description" content="Web Development Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
